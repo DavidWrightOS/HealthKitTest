fe545afcb0f8c6432cdc8bd7f18a20622f07a0d6
David Wright <davidtwright89@gmail.com>
2021-02-18 23:16:41 -0500

    Added two optional chart styles (a custom color style and a tableview header style)

--- HealthKitTest/Views/ChartView.swift
+++ HealthKitTest/Views/ChartView.swift
@@ -86,6 +86,7 @@ class ChartView: UIView {
         chartView.graphView.horizontalAxisMarkers = horizontalAxisMarkers
         chartView.tintColor = tintColor
         applyDefaultConfiguration()
+//        applyAlternateColorStyle()
         
         // Update graphView dataSeries
         let unitTitle = delegate?.chartUnitTitle ?? ""
@@ -113,10 +114,51 @@ extension ChartView {
         chartView.graphView.yMinimum = 0
     }
     
+    /// Apply alternate color configuration to set axes and style in a custom configuration.
+    private func applyAlternateColorStyle() {
+        chartView.customStyle = CustomStyle()
+        chartView.tintColor = #colorLiteral(red: 0.2941176471, green: 0.5411764706, blue: 0.6117647059, alpha: 1)
+        chartView.headerView.detailLabel.textColor = #colorLiteral(red: 0.5305851102, green: 0.5456260443, blue: 0.5923916101, alpha: 1)
+        chartView.graphView.numberFormatter = numberFormatter
+        chartView.graphView.yMinimum = 0
+    }
     
-    private let numberFormatter: NumberFormatter = {
-        let numberFormatter = NumberFormatter()
-        numberFormatter.numberStyle = .none
-        return numberFormatter
-    }()
+    /// Apply standard configuration to set axes and style for use as a header with an `.insetGrouped` tableView.
+    private func applyHeaderStyle() {
+        chartView.headerView.detailLabel.textColor = .secondaryLabel
+        chartView.customStyle = ChartHeaderStyle()
+    }
+}
+
+
+// MARK: - Custom Header Style
+
+extension ChartView {
+    /// A styler for using the chart as a header with an `.insetGrouped` tableView.
+    struct ChartHeaderStyle: OCKStyler {
+        var appearance: OCKAppearanceStyler {
+            NoShadowAppearanceStyle()
+        }
+    }
+    
+    struct NoShadowAppearanceStyle: OCKAppearanceStyler {
+        var shadowOpacity1: Float = 0
+        var shadowRadius1: CGFloat = 0
+        var shadowOffset1: CGSize = .zero
+    }
+}
+
+
+// MARK: - Custom Color Style
+
+extension ChartView {
+    /// A styler using a custom color configuration
+    struct CustomStyle: OCKStyler {
+        var color: OCKColorStyler { CustomColors() }
+    }
+    
+    struct CustomColors: OCKColorStyler {
+        var secondaryCustomGroupedBackground: UIColor { #colorLiteral(red: 0.2117647059, green: 0.2431372549, blue: 0.337254902, alpha: 1) } // chart background color
+        var label: UIColor { #colorLiteral(red: 0.8470588235, green: 0.8470588235, blue: 0.8470588235, alpha: 1) } // chart title and horizontal axis label color
+    }
 }
